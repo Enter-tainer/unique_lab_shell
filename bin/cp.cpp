b4fb1f1e3baeeaa2170d8af78b1ac47130ad4cfb
Margatroid <i@margatroid.xyz>
2019-11-17 19:56:17 +0800

    fix(cp): fix cp

--- bin/cp.cpp
+++ bin/cp.cpp
@@ -9,8 +9,26 @@ int main(int argc, char **argv) {
   if (argc != 3) {
     std::cout << "Too few or too many arguments" << std::endl;
   } else {
-    std::freopen(argv[1], "r", stdin);
-    std::freopen(argv[2], "w", stdout);
+    FILE* res = std::freopen(argv[1], "r", stdin);
+    if (res == nullptr) {
+      perror("cannot read");
+      exit(1);
+    }
+    std::string dist = argv[2], from = argv[1];
+    if (*from.rbegin() == '/')
+      from.pop_back();
+    if (*dist.rbegin() == '/')
+      dist.pop_back();
+    int del_count = 1 + from.find_last_of('/');
+    from.erase(0, del_count);
+    dist += std::string("/") + from;
+    std::cout << dist << std::endl;
+    res = std::freopen(dist.c_str(), "w", stdout);
+    
+    if (res == nullptr) {
+      perror(("cannot write" + dist).c_str());
+      exit(1);
+    }
     char c;
     while ((c = std::getchar()) != EOF) {
       putchar(c);
